32edf6492b9e745117b09f53a5175cdb55028d39
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 20:37:37 -0300

    Update ordem_servico.py

--- assistencia_tecnica/models/ordem_servico.py
+++ assistencia_tecnica/models/ordem_servico.py
@@ -1,1 +1,22 @@
+from datetime import date
+
+class OrdemServico:
+    def __init__(self, id_os, cliente, funcionario, data_emissao=None, data_inicio=None):
+        self.id_os = id_os
+        self.cliente = cliente
+        self.funcionario = funcionario
+        self.data_emissao = data_emissao or date.today()
+        self.data_inicio = data_inicio
+        self.status = "Aberta"
+        self.itens_os = []
+        self.visita_tecnica = None
+
+    def adicionar_item(self, item_os):
+        self.itens_os.append(item_os)
+
+    def agendar_visita(self, visita_tecnica):
+        self.visita_tecnica = visita_tecnica
+
+    def encerrar_os(self):
+        self.status = "Encerrada"
 
