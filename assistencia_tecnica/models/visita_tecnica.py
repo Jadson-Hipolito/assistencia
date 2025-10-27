f60fde36918e37b183a67fa2f743a831f6ff4605
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 20:37:09 -0300

    Update visita_tecnica.py

--- assistencia_tecnica/models/visita_tecnica.py
+++ assistencia_tecnica/models/visita_tecnica.py
@@ -1,1 +1,10 @@
+from datetime import datetime
+
+class VisitaTecnica:
+    def __init__(self, id_visita, os, endereco, data_hora):
+        self.id_visita = id_visita
+        self.os = os
+        self.endereco = endereco
+        self.data_hora = data_hora
+        self.status = "Agendada"
 
