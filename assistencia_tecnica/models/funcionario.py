289c07b06fdd5aa4a9aa945c90d9039a686aa444
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 20:38:52 -0300

    Update funcionario.py

--- assistencia_tecnica/models/funcionario.py
+++ assistencia_tecnica/models/funcionario.py
@@ -1,1 +1,11 @@
+class Funcionario:
+    def __init__(self, id_funcionario, nome, cpf, endereco, telefone, salario, horario_expediente, cargo):
+        self.id_funcionario = id_funcionario
+        self.nome = nome
+        self.cpf = cpf
+        self.endereco = endereco
+        self.telefone = telefone
+        self.salario = salario
+        self.horario_expediente = horario_expediente
+        self.cargo = cargo
 
