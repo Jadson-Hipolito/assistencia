0dd0fb791eb6445bad250355ad40e853bc506118
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 20:39:44 -0300

    Update cliente.py

--- assistencia_tecnica/models/cliente.py
+++ assistencia_tecnica/models/cliente.py
@@ -1,1 +1,13 @@
+class Cliente:
+    def __init__(self, id_cliente, nome, cpf, data_nascimento, endereco, telefone):
+        self.id_cliente = id_cliente
+        self.nome = nome
+        self.cpf = cpf
+        self.data_nascimento = data_nascimento
+        self.endereco = endereco
+        self.telefone = telefone
+        self.historico_servicos = []
+
+    def adicionar_ordem_servico(self, ordem_servico):
+        self.historico_servicos.append(ordem_servico)
 
