6de2f19248136013e19a74a961e9395e8862b1f1
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 20:38:02 -0300

    Update item_os.py

--- assistencia_tecnica/models/item_os.py
+++ assistencia_tecnica/models/item_os.py
@@ -1,1 +1,8 @@
+class ItemOS:
+    def __init__(self, id_item, os, equipamento, quantidade, descricao):
+        self.id_item = id_item
+        self.os = os
+        self.equipamento = equipamento
+        self.quantidade = quantidade
+        self.descricao = descricao
 
