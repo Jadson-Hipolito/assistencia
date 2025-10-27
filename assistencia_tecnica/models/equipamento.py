bb98be5b08268e2d5d01b9b9cbcd0b0d1bfb139c
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 20:38:25 -0300

    Update equipamento.py

--- assistencia_tecnica/models/equipamento.py
+++ assistencia_tecnica/models/equipamento.py
@@ -1,1 +1,8 @@
+class Equipamento:
+    def __init__(self, id_equipamento, tipo, marca, modelo, quantidade):
+        self.id_equipamento = id_equipamento
+        self.tipo = tipo
+        self.marca = marca
+        self.modelo = modelo
+        self.quantidade = quantidade
 
