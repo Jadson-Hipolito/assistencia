f362a558d344704b3f7209bbc1b71f1bd68c464c
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 21:19:58 -0300

    Delete assistencia_tecnica/models/cargo.py

--- assistencia_tecnica/models/cargo.py
+++
@@ -1,8 +0,0 @@
-class Cargo:
-    def __init__(self, id_cargo, nome_cargo):
-        self.id_cargo = id_cargo
-        self.nome_cargo = nome_cargo
-
-    def __repr__(self):
-        return f"Cargo(id={self.id_cargo}, nome={self.nome_cargo})"
-
