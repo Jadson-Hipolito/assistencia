e0af6ca33577548726ef6c8ca4ab6062a47a6d1e
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 21:03:13 -0300

    Update cliente.py

--- assistencia_tecnica/models/cliente.py
+++ assistencia_tecnica/models/cliente.py
@@ -1,16 +1,28 @@
+import json
+from pathlib import Path
+
 class Cliente:
-    def __init__(self, id_cliente, nome, cpf, data_nascimento, endereco, telefone):
+    def __init__(self, id_cliente, nome, endereco, contato, cpf, historico_servicos=None):
         self.id_cliente = id_cliente
         self.nome = nome
-        self.cpf = cpf
-        self.data_nascimento = data_nascimento
         self.endereco = endereco
-        self.telefone = telefone
-        self.historico_servicos = []
+        self.contato = contato
+        self.cpf = cpf
+        self.historico_servicos = historico_servicos or []
 
     def adicionar_ordem_servico(self, ordem_servico):
         self.historico_servicos.append(ordem_servico)
 
+    def to_dict(self):
+        return {
+            "id_cliente": self.id_cliente,
+            "nome": self.nome,
+            "endereco": self.endereco,
+            "contato": self.contato,
+            "cpf": self.cpf,
+            "historico_servicos": self.historico_servicos,
+        }
+
     def __repr__(self):
-        return f"Cliente(id={self.id_cliente}, nome={self.nome})"
+        return f"Cliente(id_cliente={self.id_cliente}, nome={self.nome})"
 
