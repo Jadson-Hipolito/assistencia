492e59d964fee1d835e9c8ad26f13ac7138a9db5
Mariana <92758993+Got7mj@users.noreply.github.com>
2025-10-27 21:07:07 -0300

    Update funcionario.py

--- assistencia_tecnica/models/funcionario.py
+++ assistencia_tecnica/models/funcionario.py
@@ -1,11 +1,26 @@
+import json
+from pathlib import Path
+
 class Funcionario:
-    def __init__(self, id_funcionario, nome, cpf, endereco, telefone, salario, horario_expediente, cargo):
+    def __init__(self, id_funcionario, nome, endereco, contato, horario, salario, cnpj, ativo=True):
         self.id_funcionario = id_funcionario
         self.nome = nome
-        self.cpf = cpf
         self.endereco = endereco
-        self.telefone = telefone
+        self.contato = contato
+        self.horario = horario
         self.salario = salario
-        self.horario_expediente = horario_expediente
-        self.cargo = cargo
+        self.cnpj = cnpj
+        self.ativo = ativo
+
+    def to_dict(self):
+        return {
+            "id_funcionario": self.id_funcionario,
+            "nome": self.nome,
+            "endereco": self.endereco,
+            "contato": self.contato,
+            "horario": self.horario,
+            "salario": self.salario,
+            "cnpj": self.cnpj,
+            "ativo": self.ativo,
+        }
 
