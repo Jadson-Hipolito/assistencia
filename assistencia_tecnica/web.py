f91a90c80dcfde8252b4d37649ba3d2d2742d067
Jadson-Hipolito <142536589+Jadson-Hipolito@users.noreply.github.com>
2025-11-05 09:35:48 -0300

    interface visita 0.1

--- assistencia_tecnica/web.py
+++ assistencia_tecnica/web.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
-from assistencia_tecnica.api.schemas.routers import interface, cliente, funcionario, equipamento, ordem_servico
+from assistencia_tecnica.api.schemas.routers import interface, cliente, funcionario, equipamento, ordem_servico, visita_tecnica
 
 app = FastAPI(title="Assistência Técnica")
 
@@ -10,6 +10,7 @@ app.include_router(cliente.router)
 app.include_router(funcionario.router)
 app.include_router(equipamento.router)
 app.include_router(ordem_servico.router)
+app.include_router(visita_tecnica.router)
 
 # Monta a pasta de arquivos estáticos
 app.mount("/static", StaticFiles(directory="assistencia_tecnica/static"), name="static")
